111edfa9b9f925fc3fad70b24e22dfa4be83aa02
marvin obig <obig.marv@gmail.com>
2021-12-06 16:03:21 +0100

    added event listeners and functions

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,5 +1,19 @@
 let rounds = parseInt(prompt("How many rounds would you like to play?"));
 
 for (let i = 0; i < rounds; i++) {
+    document.getElementById("rock").addEventListener("click", rock);
+    document.getElementById("paper").addEventListener("click", paper);
+    document.getElementById("scissors").addEventListener("click", scissors);
 
-}
+    function rock () {
+
+    };
+
+    function paper () {
+
+    };
+
+    function scissors () {
+
+    };
+};
