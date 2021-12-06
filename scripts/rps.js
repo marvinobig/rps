04378d32f7196aace8522f6b2e7c942c644b3afd
marvin obig <obig.marv@gmail.com>
2021-12-06 18:00:58 +0100

    added string manipulation for player name

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,30 +1,36 @@
 let player = prompt("What is your name?");
-let rounds = parseInt(prompt("How many rounds would you like to play?"));
+let playerN = player.slice(0, 1);
+let playerNC = playerN.toUpperCase();
+let playerN1 =player.substr(1);
+let playerN2 = playerN1.toLowerCase();
+let formatedPlayerName = playerNC + playerN2;
 
-document.getElementById("p-name").innerHTML = player;
 
-for (let i = 0; i < rounds; i++) {
-    document.getElementById("rock").addEventListener("click", rock);
-    document.getElementById("paper").addEventListener("click", paper);
-    document.getElementById("scissors").addEventListener("click", scissors);
+document.getElementById("p-name").innerHTML = formatedPlayerName;
 
-    function rock () {
-        document.getElementById("p-choice").innerHTML = "Rock";
-    };
+document.getElementById("rock").addEventListener("click", rock ());
+document.getElementById("paper").addEventListener("click", paper ());
+document.getElementById("scissors").addEventListener("click", scissors ());
 
-    function paper () {
-        document.getElementById("p-choice").innerHTML = "Paper";
-    };
-
-    function scissors () {
-        document.getElementById("p-choice").innerHTML = "Scissors";
-    };
-
-    function cpuChoice () {
-
-    };
-
-    function winner () {
-
-    };
+function rock () {
+    document.getElementById("p-choice").innerHTML = "Rock";
+    cpuChoice ();
+    document.getElementById("c-choice").innerHTML = cChoice ();
 };
+
+function paper () {
+    document.getElementById("p-choice").innerHTML = "Paper";
+};
+
+function scissors () {
+    document.getElementById("p-choice").innerHTML = "Scissors";
+};
+
+function cpuChoice () {
+
+};
+
+function winner () {
+
+};
+
