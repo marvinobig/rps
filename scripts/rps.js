4da6e232ee7785d2ed70c103dcb9e98cafde4d4b
marvin obig <obig.marv@gmail.com>
2021-12-06 16:57:26 +0100

    added final functions, added flexbox

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,4 +1,7 @@
+let player = prompt("What is your name?");
 let rounds = parseInt(prompt("How many rounds would you like to play?"));
+
+document.getElementById("p-name").innerHTML = player;
 
 for (let i = 0; i < rounds; i++) {
     document.getElementById("rock").addEventListener("click", rock);
@@ -6,14 +9,22 @@ for (let i = 0; i < rounds; i++) {
     document.getElementById("scissors").addEventListener("click", scissors);
 
     function rock () {
-
+        document.getElementById("p-choice").innerHTML = "Rock";
     };
 
     function paper () {
-
+        document.getElementById("p-choice").innerHTML = "Paper";
     };
 
     function scissors () {
+        document.getElementById("p-choice").innerHTML = "Scissors";
+    };
+
+    function cpuChoice () {
+
+    };
+
+    function winner () {
 
     };
 };
