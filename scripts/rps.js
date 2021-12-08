4b5a60d5626a06ed75764aa204f47026f5f99110
marvin obig <obig.marv@gmail.com>
2021-12-08 16:27:14 +0100

    added score tracker

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,49 +1,70 @@
-let player = prompt("What is your name?"); // askes for players name
-let playerN = player.slice(0, 1); // takes first letter of given name
-let playerNC = playerN.toUpperCase(); // capitalises first letter
-let playerN1 =player.substr(1); // takes the remaining letters of given name
-let playerN2 = playerN1.toLowerCase(); // turns remaining letters to lower case
-let formatedPlayerName = playerNC + playerN2; // combines capitalised first letter and remaining lower case letters
+// askes for players name
+let player = prompt("What is your name?");
 
+/* capitalised first letter & 
+turns remaining letters to lower case */
+let playerN = player.slice(0, 1).toUpperCase();
+let playerN1 = player.substr(1).toLowerCase();
 
-document.getElementById("p-name").innerHTML = formatedPlayerName;
+/* combines capitalised first letter 
+and remaining lower case letters */
+let formatedPlayerName = playerN + playerN1;
 
-document.getElementById("rock").addEventListener("click", rock);
-document.getElementById("paper").addEventListener("click", paper);
-document.getElementById("scissors").addEventListener("click", scissors);
+document.getElementById("p-name").textContent = formatedPlayerName;
+
+if (player === "") {
+  document.getElementById("p-name").textContent = "Unknown";
+}
 
 // if player chooses rock
+document.getElementById("rock").addEventListener("click", rock);
+
 function rock() {
-    document.getElementById("p-choice").innerHTML = "Rock";
-    
-    return cpuChoice();
-};
+  document.getElementById("p-choice").textContent = "Rock";
+  return cpuChoice();
+}
 
 // if player chooses paper
+document.getElementById("paper").addEventListener("click", paper);
+
 function paper() {
-    document.getElementById("p-choice").innerHTML = "Paper";
-    
-    return cpuChoice();
-};
+  document.getElementById("p-choice").textContent = "Paper";
+  return cpuChoice();
+}
 
 // if player chooses scissors
+document.getElementById("scissors").addEventListener("click", scissors);
+
 function scissors() {
-    document.getElementById("p-choice").innerHTML = "Scissors";
-    
-    return cpuChoice();
-};
+  document.getElementById("p-choice").textContent = "Scissors";
+  return cpuChoice();
+}
 
 /* function calculates and stores computers choice in 
 a variable */
 function cpuChoice() {
-    const CHOICES = ["Rock", "Paper", "Scissors"];
-    let cMove = CHOICES[Math.floor(Math.random()*CHOICES.length)];
+  const CHOICES = ["Rock", "Paper", "Scissors"];
+  let cMove = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  document.getElementById("c-choice").textContent = cMove;
 
-    document.getElementById("c-choice").innerHTML = cMove;
-    
-};
+  playerChoice = document.getElementById("p-choice").textContent;
+  score = document.getElementById("p-score").textContent;
+  score2 = document.getElementById("c-score").textContent;
+  playerScore = parseInt(score);
+  cpuScore = parseInt(score2);
 
-function winner () {
-
-};
-
+  //score tracker
+  if (playerChoice === "Rock" && cMove === "Scissors") {
+    document.getElementById("p-score").textContent = ++playerScore;
+  } else if (playerChoice === "Scissors" && cMove === "Rock") {
+    document.getElementById("c-score").textContent = ++cpuScore;
+  } else if (playerChoice === "Paper" && cMove === "Rock") {
+    document.getElementById("p-score").textContent = ++playerScore;
+  } else if (playerChoice === "Rock" && cMove === "Paper") {
+    document.getElementById("c-score").textContent = ++cpuScore;
+  } else if (playerChoice === "Scissors" && cMove === "Paper") {
+    document.getElementById("p-score").textContent = ++playerScore;
+  } else if (playerChoice === "Paper" && cMove === "Scissors") {
+    document.getElementById("c-score").textContent = ++cpuScore;
+  }
+}
